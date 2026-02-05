2b725ee0a6bf4279ca2eb7ca77c08d60d76ca8f7
Fabian <>
2026-02-05 15:34:04 +0100

    weather check challenge

---
+++ js-conditions-and-booleans/weather-check/js/index.js
@@ -0,0 +1,36 @@
+// Change this value to test different "weather" conditions.
+const weather = "sunny";
+
+switch (weather) {
+    case "sunny":
+        console.log("It's sunny!");
+        break;
+    case "rainy":
+        console.log("It's raining, don't forget your umbrella!");
+        break;
+    case "snowy":
+        console.log("It's snowing, stay warm!");
+        break;
+    default:
+        "I'm not sure what the weather is like.";
+}
+
+// Adjust the "temperature" value to trigger different messages (for Part 2 only).
+const temperature = 10;
+
+switch (true) {
+    case weather === "sunny" && temperature > 20:
+        console.log("It's sunny and warm!");
+        break;
+    case weather === "sunny" && temperature <= 20:
+        console.log("It's sunny, but a bit chilly!");
+        break;
+    case weather === "rainy":
+        console.log("It's raining, don't forget your umbrella!");
+        break;
+    case weather === "snowy" && temperature < 0:
+        console.log("Freezing snow!");
+        break;
+    default:
+        "I'm not sure what the weather is like.";
+}
