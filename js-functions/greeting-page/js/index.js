27fab26ebb91a8d74214300eb2d10d998330ba44
Fabian <>
2026-02-06 16:31:15 +0100

    greeting page challenge

---
+++ js-functions/greeting-page/js/index.js
@@ -0,0 +1,52 @@
+console.clear();
+
+/*
+Update the content and style of the page based on the current day and time.
+
+- Write a function `getGreeting` that returns a different greeting depending on the current time:
+  - 6 - 12: returns "Good Morning"
+  - 13 - 18: returns "Good Afternoon"
+  - 19 - 22: returns "Good Evening"
+  - 23 - 5: returns "Good Night"
+
+(HINT: You can get the current hour with `new Date().getHours()`)
+
+- Write a function `getDayColor` that returns a different color depending on the current weekday:
+  - Monday: "darkgray"
+  - Tuesday - Friday: "lightblue"
+  - Saturday - Sunday: "hotpink"
+
+(HINT: You can get the current weekday with `new Date().getDay()`)
+
+*/
+
+const display = document.querySelector('[data-js="display"]');
+
+function getGreeting() {
+    // Code here
+    const hours = new Date().getHours();
+    if (hours >= 6 && hours <= 12) {
+        return "Good Morning";
+    } else if (hours >= 13 && hours <= 18) {
+        return "Good Afternoon";
+    } else if (hours >= 13 && hours <= 18) {
+        return "Good Evening";
+    } else {
+        return "Good Night";
+    }
+}
+
+function getDayColor() {
+    // Code here
+    const day = new Date().getDay();
+    if (day == 1) {
+        return "darkgray";
+    } else if (day >= 2 && day <= 5) {
+        return "lightblue";
+    } else {
+        return "hotpink";
+    }
+}
+
+display.textContent = getGreeting();
+document.body.style.backgroundColor = getDayColor();
