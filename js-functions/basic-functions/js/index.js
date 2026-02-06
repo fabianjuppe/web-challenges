34f5ff90d8ea6c55ab21a7df69d5d2041e59e1d5
Fabian <>
2026-02-06 16:28:45 +0100

    basic functions challenge

---
+++ js-functions/basic-functions/js/index.js
@@ -0,0 +1,66 @@
+console.clear();
+
+/*
+1: Create the data for a book in an online store. Define variables for the following details:
+  - The title of the book
+  - The author of the book
+  - The book's rating
+  - The number of copies sold
+*/
+
+// --v-- write your code here --v--
+const title = "The Lord of the Javascript";
+const author = "Mario";
+let rating = 4.2;
+let sales = 120;
+// --^-- write your code here --^--
+
+/*
+2: Log all variables to the console, for example:
+
+Title: The Lord of the Javascript
+Author: Mario
+Rating: 4.2
+Sales: 120
+
+Then:
+- Increase the number of sales.
+- Update the book's rating.
+- Log all variables to the console again after making these updates.
+*/
+
+// --v-- write your code here --v--
+console.log(
+    `Title: ${title}\n Author: ${author}\n Rating: ${rating}\n Sales: ${sales}`,
+);
+
+sales = 250;
+rating = 3.9;
+
+console.log(
+    `Title: ${title}\n Author: ${author}\n Rating: ${rating}\n Sales: ${sales}`,
+);
+// --^-- write your code here --^--
+
+/*
+3: The logging code above is repetitive and hard to maintain.
+   Refactor your code by doing the following:
+
+ - Write a function called `logBookData` that logs all the book details to the console.
+ - Replace the existing `console.log` statements with calls to this function.
+ - Then, increase the number of sales two more times and log the updated details after each increase.
+*/
+
+// --v-- write your code here --v--
+function logBookData() {
+    console.log(
+        `Title: ${title}\n Author: ${author}\n Rating: ${rating}\n Sales: ${sales}`,
+    );
+}
+
+logBookData();
+sales = 8000;
+rating = 4.8;
+logBookData();
+
+// --^-- write your code here --^--
