281e76c38c5d16e70c23443e83464faf0a497eff
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-02-11 11:13:35 +0100

    social media post challenge

---
+++ js-createelement/social-media-post/js/index.js
@@ -0,0 +1,37 @@
+console.clear();
+
+function handleLikeButtonClick(event) {
+    const buttonElement = event.target;
+    buttonElement.classList.toggle("post__button--liked");
+}
+
+const likeButton = document.querySelector('[data-js="like-button"]');
+likeButton.addEventListener("click", handleLikeButtonClick);
+
+// Exercise:
+// Use document.createElement() and append another social media post to the body.
+const article = document.createElement("article");
+article.classList.add("post");
+document.body.append(article);
+
+const p = document.createElement("p");
+p.classList.add("post__content");
+p.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+article.append(p);
+
+const footer = document.createElement("footer");
+footer.classList.add("post__footer");
+article.append(footer);
+
+const span = document.createElement("span");
+span.classList.add("post__username");
+span.textContent = "@username";
+footer.append(span);
+
+const button = document.createElement("button");
+button.type = "button";
+button.classList.add("post__button");
+button.setAttribute("data-js", "like-button");
+button.addEventListener("click", handleLikeButtonClick);
+button.textContent = "♥ Like";
+footer.append(button);
