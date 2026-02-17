7e938c9bf807ebc1fceef5861d8f2ce17b0d5515
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-02-17 16:50:14 +0100

    star wars console challenge

---
+++ js-fetch/star-wars-console/js/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+const url = "https://swapi.py4e.com/api/people";
+
+async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.results);
+    console.log(data.results[2].eye_color);
+}
+
+fetchData();
