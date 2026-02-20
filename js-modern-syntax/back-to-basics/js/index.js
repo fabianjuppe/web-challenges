657051368dc9d834f0cc5fbac36d105e785784fd
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-02-20 16:46:56 +0100

    back to basics challenge

---
+++ js-modern-syntax/back-to-basics/js/index.js
@@ -0,0 +1,27 @@
+/*
+Now that you've practiced destructuring, default parameters, and the spread operator,
+try reversing the challenge by rewriting this code without using these modern features.
+*/
+
+export function getNameAndCountry(city) {
+    return [city.name, city.country];
+}
+
+export function getRelocatedCity(city1, city2) {
+    if (city2 === undefined) {
+        city2 = { name: "Berlin", country: "Germany" };
+    }
+
+    const nameAndCountry = getNameAndCountry(city2);
+    const country = nameAndCountry[1];
+
+    const relocatedCity = {};
+    for (const key in city1) {
+        if (city1.hasOwnProperty(key)) {
+            relocatedCity[key] = city1[key];
+        }
+    }
+
+    relocatedCity.country = country;
+    return relocatedCity;
+}
