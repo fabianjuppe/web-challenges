81d25c2d48630f41608a40df5883e1ebc923e5b6
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-02-26 12:16:23 +0100

    hello world article challenge

---
+++ react-basics/hello-world-article/src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+    return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+    return (
+        <article>
+            <h1>Example Heading</h1>
+            <p>Example Text</p>
+        </article>
+    );
+}
