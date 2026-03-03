40aed45b2b9d20aa444473d3eec7a53ff54927f8
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-03 13:31:23 +0100

    table reservation challenge

---
+++ react-state-2/table-reservation/src/App.jsx
@@ -0,0 +1,23 @@
+import Counter from "./components/Counter";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+    const [people, setPeople] = useState(0);
+
+    function add() {
+        setPeople(people + 1);
+    }
+
+    function subtract() {
+        setPeople(people - 1);
+    }
+
+    return (
+        <div className="container">
+            <h1>Place a Table Reservation</h1>
+            <Counter onAdd={add} onSubtract={subtract} />
+            <p>You are going to reserve a table for {people} people.</p>
+        </div>
+    );
+}
