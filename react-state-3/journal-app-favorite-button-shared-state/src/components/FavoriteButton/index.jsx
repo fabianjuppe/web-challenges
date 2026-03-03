3cb1110df4307c6f1b1d77e36cd0b02dfcb46639
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-03 13:34:37 +0100

    journal app favorite button shared state challenge

---
+++ react-state-3/journal-app-favorite-button-shared-state/src/components/FavoriteButton/index.jsx
@@ -0,0 +1,18 @@
+/* import { useState } from "react"; */
+import "./FavoriteButton.css";
+import StarFilled from "./star-filled.svg?react";
+import Star from "./star.svg?react";
+
+export default function FavoriteButton({ onToggleFavorite, isFavorite, id }) {
+    /* const [isFavorite, setIsFavorite] = useState(false); */
+
+    return (
+        <button
+            className="favorite-button"
+            onClick={() => onToggleFavorite(id)}
+            aria-label="favorite"
+        >
+            {isFavorite ? <StarFilled /> : <Star />}
+        </button>
+    );
+}
