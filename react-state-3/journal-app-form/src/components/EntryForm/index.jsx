c967e0f827485d3041729864c8d5d7c6b5c0c129
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-03 13:34:07 +0100

    journal app form challenge

---
+++ react-state-3/journal-app-form/src/components/EntryForm/index.jsx
@@ -0,0 +1,35 @@
+import "./EntryForm.css";
+import Button from "../Button";
+
+export default function EntryForm({ onAddEntry }) {
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData);
+        const entry = { motto: data.motto, notes: data.notes };
+        onAddEntry(entry);
+        form.reset();
+        form.elements.motto.focus();
+    }
+
+    return (
+        <form className="entry-form" onSubmit={handleSubmit}>
+            <h2 className="entry-form__title">New Entry</h2>
+            <div className="entry-form__fields">
+                <div className="entry-form__field">
+                    <label htmlFor="motto">Motto</label>
+                    <input type="text" name="motto" id="motto" />
+                </div>
+                <div className="entry-form__field">
+                    <label htmlFor="notes">Notes</label>
+                    <textarea name="notes" id="notes" rows="4" />
+                </div>
+                <div className="entry-form__button-wrapper">
+                    <Button type="submit">Create</Button>
+                </div>
+            </div>
+        </form>
+    );
+}
