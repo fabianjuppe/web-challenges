8bb0f96bce868145c7f8a34fc08e35684e6efc59
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-05 10:32:24 +0100

    pokemon api with paging challenge

---
+++ react-effects-and-fetch/pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonList() {
+    const [pokemon, setPokemon] = useState([]);
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        async function loadPokemon() {
+            try {
+                const response = await fetch(
+                    `https://pokeapi.co/api/v2/pokemon?offset=${offset}`,
+                );
+                const data = await response.json();
+                setPokemon(data.results);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        loadPokemon();
+    }, [offset]);
+
+    return (
+        <main>
+            <button
+                type="button"
+                onClick={() => {
+                    if (offset >= 20) {
+                        setOffset(offset - 20);
+                    }
+                }}
+                disabled={offset < 20}
+            >
+                Previous Page
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    setOffset(offset + 20);
+                }}
+            >
+                Next Page
+            </button>
+            <ul>
+                {pokemon.map(({ name }) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+        </main>
+    );
+}
