2874d45ff3ee1544eefc211b0c420574bdeeab38
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-17 11:53:09 +0100

    box challenge

---
+++ react-styled-components/box/pages/index.js
@@ -0,0 +1,18 @@
+import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "@/components/BoxWithStyledComponents/BoxWithStyledComponents.js";
+import styled from "styled-components";
+
+export default function HomePage() {
+    return (
+        <FlexContainer>
+            <BoxWithClassName />
+            <BoxWithClassName isBlack />
+            <BoxWithStyledComponents />
+            <BoxWithStyledComponents $isBlack />
+        </FlexContainer>
+    );
+}
+
+const FlexContainer = styled.div`
+    display: flex;
+`;
